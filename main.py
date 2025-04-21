665b32c85d42e213ca9589d2d99c1b14b8a8ed02
louisvs1993 <louisvansteelant@hotmail.com>
2025-04-21 16:14:52 +0200

    asteroid field added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,10 +11,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     dt = 0
 
@@ -31,7 +37,7 @@ def main():
         for obj in drawable:
             obj.draw(screen)
 
-        #REFRESH
+        #REFRESH SCREEN
         pygame.display.flip()
 
         #Pause gameloop until 1/60th of a second has passed, devided that output with 1000 and save it in dt variable
