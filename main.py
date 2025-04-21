dbc48bb4bfb0f0d057dcbdd6661f7e935e07649e
louisvs1993 <louisvansteelant@hotmail.com>
2025-04-21 14:47:20 +0200

    added fps limiter

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,9 @@ def main():
 
         #REFRESH
         pygame.display.flip()
+
+        #Pause gameloop until 1/60th of a second has passed, devided that output with 1000 and save it in dt variable
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
